5d97a06256f3500422133b9d8cb5710a3b982b84
DIR\j.pacheco.castro <j.pacheco.castro@accenture.com>
2018-06-08 17:04:51 +0200

    bookrecedgeservice added

---
+++ bookrecedgeservice/src/main/java/deors/demos/microservices/bookrecedgeservice/Book.java
@@ -0,0 +1,33 @@
+@Entity
+public class Book {
+
+    private Long id;
+    private String title;
+    private String author;
+
+    public Book(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
